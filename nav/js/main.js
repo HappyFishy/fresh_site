a0c2e211473db4678d3bf2b28092da88f1ee15f6
Mike <Happy Fish>
2015-04-11 17:18:10 -0400

    Bunch of pointless stuff :)

--- nav/js/main.js
+++ nav/js/main.js
@@ -2,14 +2,14 @@ jQuery(document).ready(function($){
 	var isLateralNavAnimating = false;
 	
 	//open/close lateral navigation
-	$('.navi-trigger').on('click', function(event){
+	$('.cd-nav-trigger').on('click', function(event){
 		event.preventDefault();
 		//stop if nav animation is running 
 		if( !isLateralNavAnimating ) {
 			if($(this).parents('.csstransitions').length > 0 ) isLateralNavAnimating = true; 
 			
 			$('body').toggleClass('navigation-is-open');
-			$('.navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			$('.cd-navigation-wrapper').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
 				//animation is over
 				isLateralNavAnimating = false;
 			});
